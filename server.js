aea4ce375498b8ae74c4e945f10dd929e458a4c6
delodcyber <Truebasisoflife@yahoo.com>
2026-06-30 16:39:29 +0100

    feat: init instances installed

---
+++ server.js
@@ -0,0 +1,12 @@
+
+
+async function main() {
+	// we'll add code here soon
+}
+
+
+/**
+ * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+ * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+ */
+const uri = "mongodb+srv://johnny07:<db_password>@cluster0.wtjdlf7.mongodb.net/?appName=Cluster0";
