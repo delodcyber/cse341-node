07e83d9e17d9d7620ce8e597eada2013e44a09ff
delodcyber <Truebasisoflife@yahoo.com>
2026-07-02 19:03:49 +0100

    fix: move my db credentials to backend.

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
-const URI = 
-"mongodb+srv://john_db:qwerty123456@cluster0.iz5qvn2.mongodb.net/?appName=Cluster0";
+const URI = process.env.MongoDB_URI;
 
 const connectDB = async () => {
     await mongoose.connect(URI), {
